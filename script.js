b7a4df6e223d4feb32ff98e351fad1e7eec5159e
JackM88 <jackmoree88@gmail.com>
2024-09-27 20:42:01 -0400

    Intial Update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// -----------------search-engine-logic----------------
 const apikey = '79ad6138e9e64f60b4ad4f94a02a966f'
 
 const blogContainer = document.getElementById("blog-container");
@@ -89,3 +90,45 @@ function displayBlogs(articles) {
         console.error("Error Fetching random news", error);
     }
 })();
+
+// ---------------------home-page-logic---------------
+var tablinks = document.getElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+
+        function opentab(tabname){
+            for(tablink of tablinks){
+                tablink.classList.remove("active-link");
+            }
+            for(tabcontent of tabcontents){
+                tabcontent.classList.remove("active-tab");
+            }
+            event.currentTarget.classList.add("active-link");
+            document.getElementById(tabname).classList.add("active-tab");
+        }
+
+
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbzYfvpOtZOhUtVRGku3vdqy3x0jLXlOc9_HOxgCKGKCR-km0xhPqU-oFlSjq5od_ox2DQ/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById('msg')
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Submission received!"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
+
+    var sidemenu = document.getElementById("sidemenu");
+
+        function openmenu(){
+            sidemenu.style.right = "0";
+        }
+        function closemenu(){
+            sidemenu.style.right = "-200px";
+        }
